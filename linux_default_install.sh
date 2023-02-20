c713d15fef573c2a38f9c691fefceae161534f3d
Lukas <git-commits@lukas-schieren.de>
2023-02-20 08:04:57 +0100

    feat: Adds Linux Install Script

---
+++ linux_default_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt update -y && sudo apt autoremove -y && sudo apt autoclean -y
+sudo apt install -y firefox thunderbird vim vi vlc dbeaver-ce wireshark obs-studio arduino veracrypt texlive-full git curl pdfsam bleachbit keepassxc flameshot libreoffice git xjournal timeshift htop
