66957e1475148d7d1e0211ea618381d3cd22df29
Lukas <git-commits@lukas-schieren.de>
2023-02-20 06:16:08 +0100

    feat: Adds Windows bloatware uninstall scripts.

---
+++ windows_delete_bloatware_Before_Build_2004_05_2020.ps1
@@ -0,0 +1,32 @@
+# For Windows 10 before Build 2004 / May 2020 
+
+Get-AppxPackage -allusers *3dbuilder* | Remove-AppxPackage #3D Builder
+Get-AppxPackage -allusers *windowsalarms* | Remove-AppxPackage # Alarm und Uhr
+Get-AppxPackage -allusers *windowsphone* | Remove-AppxPackage # Begleiter für Telefon
+Get-AppxPackage -allusers *DrawboardPDF* | Remove-AppxPackage # Drawboard PDF 
+Get-AppxPackage -allusers *getstarted* | Remove-AppxPackage # Erste Schritte
+Get-AppxPackage -allusers *Facebook* | Remove-AppxPackage # Facebook
+Get-AppxPackage -allusers *feedback* | Remove-AppxPackage # Feedback Hub
+Get-AppxPackage -allusers *zunevideo* | Remove-AppxPackage # Filme & TV
+Get-AppxPackage -allusers *bingfinance* | Remove-AppxPackage # Finanzen
+Get-AppxPackage -allusers *photo* | Remove-AppxPackage # Fotos
+Get-AppxPackage -allusers *zunemusic* | Remove-AppxPackage # Groove-Musik
+Get-AppxPackage -allusers *windowscamera* | Remove-AppxPackage # Kamera
+Get-AppxPackage -allusers *windowsmaps* | Remove-AppxPackage # Karten
+Get-AppxPackage -allusers *people* | Remove-AppxPackage # Kontakte
+Get-AppxPackage -allusers *communicationsapps* | remove-appxpackage # Mail und Kalender
+Get-AppxPackage -allusers *solitairecollection* | Remove-AppxPackage # Microsoft Solitaire Collection
+Get-AppxPackage -allusers *bingnews* | Remove-AppxPackage # Nachrichten
+Get-AppxPackage -allusers *messaging* | remove-appxpackage # Nachrichten & Skype
+Get-AppxPackage -allusers *officehub* | Remove-AppxPackage # Office holen
+Get-AppxPackage -allusers *onenote* | Remove-AppxPackage # OneNote
+Get-AppxPackage -allusers *mspaint* | Remove-AppxPackage # Paint 3D
+Get-AppxPackage -allusers *windowscalculator* | Remove-AppxPackage # Rechner
+Get-AppxPackage -allusers *skypeapp* | Remove-AppxPackage # Skype
+Get-AppxPackage -allusers *bingsports* | Remove-AppxPackage # Sport
+Get-AppxPackage -allusers *soundrecorder* | Remove-AppxPackage # Sprachrekorder
+Get-AppxPackage -allusers *dvd* | Remove-AppxPackage # Windows DVD Player
+Get-AppxPackage -allusers *xboxIdentityprovider* | Remove-AppxPackage # Xbox Identity Provider
+Get-AppxPackage -allusers *xboxapp* | Remove-AppxPackage # Xbox
+Get-AppxPackage -allusers *Asphalt8Airborne* | Remove-AppxPackage # Asphalt 8: Airborne
+Get-AppxPackage -allusers *CandyCrushSaga* | Remove-AppxPackage # Candy Crush Saga
