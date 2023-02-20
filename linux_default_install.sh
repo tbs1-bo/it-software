2b177ec2e8f3441e0761c74b715b768c4bb8446c
Lukas <git-commits@lukas-schieren.de>
2023-02-20 08:09:13 +0100

    fixup: remove duplicate, add wget

--- linux_default_install.sh
+++ linux_default_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt update -y && sudo apt autoremove -y && sudo apt autoclean -y
-sudo apt install -y firefox thunderbird vim vi vlc dbeaver-ce wireshark obs-studio arduino veracrypt texlive-full git curl pdfsam bleachbit keepassxc flameshot libreoffice git xjournal timeshift htop
+sudo apt install -y firefox thunderbird vim vi vlc dbeaver-ce wireshark obs-studio arduino veracrypt texlive-full git curl pdfsam bleachbit keepassxc flameshot libreoffice wget xjournal timeshift htop
